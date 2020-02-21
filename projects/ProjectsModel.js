f1f226c752152bc0c6b7d756689e78b757f37a2a
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-21 14:49:15 -0600

    MVP

--- projects/ProjectsModel.js
+++ projects/ProjectsModel.js
@@ -2,7 +2,12 @@ const projectsDB = require('../data/db-config.js');
 
 module.exports = {
   getAllProjects,
-  addProject
+  addProject,
+  getAllResources,
+  addResource,
+  getAllTasks,
+  addTask,
+  updateProjectTask
 }
 
 function getAllProjects() {
@@ -14,5 +19,37 @@ function addProject(project) {
     .insert(project)
     .then(project => {
       return project;
+    })
+}
+
+function getAllResources() {
+  return projectsDB('resources');
+}
+
+function addResource(resource) {
+  return projectsDB('resources')
+    .insert(resource)
+    .then(resource => {
+      return resource;
+    })
+}
+
+function getAllTasks() {
+  return projectsDB('tasks')
+}
+
+function addTask(task) {
+  return projectsDB('tasks')
+    .insert(task)
+    .then(task => {
+      return task;
+    })
+}
+
+function updateProjectTask(project_id, task_id) {
+  return projectsDB('projects_tasks')
+    .insert({ project_id, task_id })
+    .then(project_task => {
+      console.log(project_task);
     })
 }
--- projects/ProjectsRouter.js
+++ projects/ProjectsRouter.js
@@ -37,4 +37,69 @@ router.post('/', (req, res) => {
   }
 })
 
+// get all resources
+router.get('/resources', (req, res) => {
+  Projects.getAllResources()
+    .then(resources => {
+      res.status(200).json(resources)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'Error getting all resources.' })
+    })
+})
+
+// post new resource
+router.post('/resources', (req, res) => {
+  const newResourceData = req.body;
+
+  if(!newResourceData.name) {
+    res.status(400).json({ errorMessage: 'Please provide a name for the resource!' })
+  } else {
+    Projects.addResource(newResourceData)
+      .then(resource => {
+        res.status(201).json(resource)
+      })
+  }
+})
+
+// get all tasks
+router.get('/tasks', (req, res) => {
+  Projects.getAllTasks()
+    .then(tasks => {
+      tasks.map(task => {
+        if(task.completed === 1) {
+          task.completed = true;
+        } else {
+          task.completed = false;
+        }
+      });
+      res.status(200).json(tasks)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({ errorMessage: 'Error getting all tasks.' })
+    })
+})
+
+// add new task
+router.post('/:id/tasks', (req, res) => {
+  const id = req.params.id;
+  const newTaskData = req.body;
+
+  if(!newTaskData.description) {
+    res.status(400).json({ errorMessage: 'Please provide a description for this task! '})
+  } else {
+    Projects.addTask(newTaskData)
+      .then(task => {
+        res.status(201).json(task);
+        Projects.updateProjectTask(id, task.id)
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ errorMessage: 'Error adding task to project' })
+      })
+  }
+})
+
 module.exports = router;
